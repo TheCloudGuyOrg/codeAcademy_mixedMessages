92d48860debf6b5dc8f6d6beea40c5bebdc20e3f
thecloudguyllc <derek.ryan.cox@thecloudguy.org>
2023-01-13 15:54:06 -0700

    Setup Message Arrays

---
+++ mixedMessages.js
@@ -0,0 +1,7 @@
+const messages = {
+    signInfo : ['star', 'moon', 'sun', 'comet'],
+    fortuneOutput : ['terrible luck', 'bad luck', 'ok luck', 'good luck', 'great luck'],
+    advice : ['go out and eat', 'not read this', 'play more', 'trust no one']
+}
+
+console.log(messages.advice);
